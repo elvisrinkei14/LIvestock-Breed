e64d8ded47449e372ada150a806ee36fe5d79e56
Moringa School <elvis.rinkei@moringaschool.com>
2022-10-14 14:59:59 +0300

    modified css and html

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ menu.addEventListener('click', function(){
     menu.classList.toggle('is-active');
     menulinks.classList.toggle('active');
 });
+
+const countapi = require('countapi-js');
+countapi.visits().then((result) => {
+    console.log(result.value);
+  });
